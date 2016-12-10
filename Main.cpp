bb8daef3b29d11a2c0071c121e537212f2b2cc30
Harold Mendoza <hrmendoza96@unitec.edu>
2016-12-09 21:27:52 -0600

    Commit 1

---
+++ Main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <ncurses.h>
+#include <stdlib.h>
+#include <string>
+#include <sstream>
+#include <vector>
+
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  initscr();
+  raw();
+
+
+
+  
+
+  getch();
+  endwin();
+  return 0;
+}
